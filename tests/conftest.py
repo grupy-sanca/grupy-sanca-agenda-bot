deaecf41b1a78a310c36b65ec666330e542c72cc
zsinx6 <zsinx6@users.noreply.github.com>
2024-12-05 13:53:08 -0300

    Minor fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,10 @@ from pytest import fixture
 @fixture
 def meetup_homepage_soup():
     with open("tests/fixtures/meetup_homepage_example.html") as f:
-        return BeautifulSoup(f.read(), "html.parser")
+        return BeautifulSoup(f, "html.parser")
 
 
 @fixture
 def meetup_event_page_soup():
     with open("tests/fixtures/meetup_event_page_example.html") as f:
-        return BeautifulSoup(f.read(), "html.parser")
+        return BeautifulSoup(f, "html.parser")
