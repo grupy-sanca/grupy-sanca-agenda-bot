f1aec3c646a35f2632db975eb0197b402c37a271
zsinx6 <zsinx6@users.noreply.github.com>
2024-12-09 11:14:56 -0300

    Remove logs

--- grupy_sanca_agenda_bot/bot.py
+++ grupy_sanca_agenda_bot/bot.py
@@ -1,14 +1,8 @@
-import logging
-
 from telegram.ext import Application, CommandHandler
 
 from grupy_sanca_agenda_bot.commands import agenda, force_update, next, start
 from grupy_sanca_agenda_bot.scheduler import setup_scheduler
 from grupy_sanca_agenda_bot.settings import settings
-
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
 
 
 def bot():
