8cb36334f6d4bc923355de1a9d4088d8372fc67c
zsinx6 <zsinx6@users.noreply.github.com>
2024-12-12 15:10:56 -0300

    Fix broken event_loop

--- grupy_sanca_agenda_bot/bot.py
+++ grupy_sanca_agenda_bot/bot.py
@@ -1,14 +1,20 @@
+import asyncio
+import logging
+
 from telegram.ext import Application, CommandHandler
 
 from grupy_sanca_agenda_bot.commands import agenda, force_update, next, start
 from grupy_sanca_agenda_bot.scheduler import setup_scheduler
 from grupy_sanca_agenda_bot.settings import settings
 
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
 
 def bot():
     application = Application.builder().token(settings.TELEGRAM_BOT_TOKEN).build()
 
-    setup_scheduler(application)
+    loop = asyncio.get_event_loop()
+    setup_scheduler(application, loop)
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("proximo", next))
